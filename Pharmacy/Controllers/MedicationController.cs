58f229cb8400a601a145d76cb290f12e0de8cc4c
Mateusz Chorab <chorobcia09@gmail.com>
2026-06-28 18:04:42 +0200

    zadanie1

--- Pharmacy/Controllers/MedicationController.cs
+++ Pharmacy/Controllers/MedicationController.cs
@@ -7,5 +7,23 @@ namespace Pharmacy.Controllers;
 [Route("[controller]")]
 public class MedicationController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var medications = MedicationData.Medications;
+        return Ok(medications);
+    }
 
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var medication = MedicationData.Medications.FirstOrDefault(m => m.Id == id);
+
+        if (medication == null)
+        {
+            return NotFound(new { Message = $"Lek o identyfikatorze {id} nie został znaleziony." });
+        }
+
+        return Ok(medication);
+    }
 }
